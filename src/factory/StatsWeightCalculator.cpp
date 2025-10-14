c14515c1117cb76561b3bd7a10ef358c14137b03
kikiviki <79366880+kikiviki@users.noreply.github.com>
2025-10-14 10:18:11 -0400

    same code - less ugly braces

--- src/factory/StatsWeightCalculator.cpp
+++ src/factory/StatsWeightCalculator.cpp
@@ -94,16 +94,14 @@ float StatsWeightCalculator::CalculateItem(uint32 itemId, int32 randomPropertyId
     if (enable_quality_blend_)
     {
         // Heirloom items scale with player level
+        // Use player level as effective item level for heirlooms - Quality EPIC
+        // Else - Blend with item quality and level for normal items
         if (proto->Quality == ITEM_QUALITY_HEIRLOOM)
-        {
-            // Use player level as effective item level for heirlooms - Quality EPIC
             weight_ *= PlayerbotFactory::CalcMixedGearScore(lvl, ITEM_QUALITY_EPIC);
-        }
+        
         else
-        {
-            // Blend with item quality and level for normal items
             weight_ *= PlayerbotFactory::CalcMixedGearScore(proto->ItemLevel, proto->Quality);
-        }
+
         return weight_;
     }
 
