7179b6356f126fa58f076915693bf4005a52fb47
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2025-11-09 18:52:47 +0100

    remove tab

--- src/strategy/actions/MovementActions.cpp
+++ src/strategy/actions/MovementActions.cpp
@@ -320,7 +320,7 @@ bool MovementAction::MoveTo(uint32 mapId, float x, float y, float z, bool idle, 
             bool allowPath = generatePath;
             if (isFlying(bot) || bot->HasUnitMovementFlag(MOVEMENTFLAG_CAN_FLY))
                 allowPath = false;
-			
+            
             mm.Clear();
             if (!backwards)
             {
