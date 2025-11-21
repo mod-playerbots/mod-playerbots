f224a8b321f41c309801d0df8de8c7957137e88b
crow <pengchengw@me.com>
2025-11-21 16:54:03 -0600

    Fix bad operator

--- src/strategy/raids/obsidiansanctum/RaidOsTriggers.cpp
+++ src/strategy/raids/obsidiansanctum/RaidOsTriggers.cpp
@@ -64,7 +64,7 @@ bool SartharionDpsTrigger::IsActive()
 
 bool SartharionMeleePositioningTrigger::IsActive()
 {
-    if (botAI->IsMeleeDps(bot)) { return false; }
+    if (!botAI->IsMeleeDps(bot)) { return false; }
 
     Unit* boss = AI_VALUE2(Unit*, "find target", "sartharion");
     if (!boss) { return false; }
