6efb0770ce6c27406d223b8425832d693fb513da
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-01-06 22:02:59 -0800

    finish refactor

--- src/strategy/rpg/NewRpgBaseAction.cpp
+++ src/strategy/rpg/NewRpgBaseAction.cpp
@@ -3,6 +3,7 @@
 #include "BroadcastHelper.h"
 #include "ChatHelper.h"
 #include "Creature.h"
+#include "FlightMasterCache.h"
 #include "G3D/Vector2.h"
 #include "GameObject.h"
 #include "GossipDef.h"
