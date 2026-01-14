58888a7238a9ebd6ecc8216881d88b8ff5f82264
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2026-01-14 14:10:29 +0100

    fix timer error

--- src/strategy/raids/naxxramas/RaidNaxxBossHelper.h
+++ src/strategy/raids/naxxramas/RaidNaxxBossHelper.h
@@ -62,7 +62,7 @@ public:
         {
             return false;
         }
-        _timer = _event_map->GetTimer();
+        _timer = getMSTime();
         return true;
     }
     virtual void Reset()
