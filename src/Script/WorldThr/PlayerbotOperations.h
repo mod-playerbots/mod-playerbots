7e8b9bb70b5ab882f3e6e7818aa8cf197e101558
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2026-01-19 12:29:39 +0100

    Extend SummonWhenGroup to auto-added bots

--- src/Script/WorldThr/PlayerbotOperations.h
+++ src/Script/WorldThr/PlayerbotOperations.h
@@ -14,9 +14,11 @@
 #include "PlayerbotOperation.h"
 #include "Player.h"
 #include "PlayerbotAI.h"
+#include "PlayerbotAIConfig.h"
 #include "PlayerbotMgr.h"
 #include "PlayerbotRepository.h"
 #include "RandomPlayerbotMgr.h"
+#include "UseMeetingStoneAction.h"
 #include "WorldSession.h"
 #include "WorldSessionMgr.h"
 
@@ -74,6 +76,22 @@ public:
         if (group->AddMember(target))
         {
             LOG_DEBUG("playerbots", "GroupInviteOperation: Successfully added {} to group", target->GetName());
+            if (sPlayerbotAIConfig->summonWhenGroup && target->GetDistance(bot) > sPlayerbotAIConfig->sightDistance)
+            {
+                PlayerbotAI* targetAI = sPlayerbotsMgr->GetPlayerbotAI(target);
+                if (targetAI)
+                {
+                    class GroupSummonAction : public SummonAction
+                    {
+                    public:
+                        GroupSummonAction(PlayerbotAI* botAI) : SummonAction(botAI, "group summon") {}
+                        using SummonAction::Teleport;
+                    };
+
+                    GroupSummonAction summonAction(targetAI);
+                    summonAction.Teleport(bot, target, true);
+                }
+            }
             return true;
         }
         else
