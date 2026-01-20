d8d95e69c1da6e37b5dd861542aff464cb06d90a
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-01-20 13:16:22 -0800

    dontfilter

--- src/Ai/Base/Actions/EquipAction.cpp
+++ src/Ai/Base/Actions/EquipAction.cpp
@@ -402,24 +402,6 @@ bool EquipUpgradesTriggeredAction::Execute(Event event)
             return false;
     }
 
-    else if (event.GetSource() == "loot roll won")
-    {
-        WorldPacket p(event.getPacket());
-        p.rpos(0);
-        ObjectGuid itemGuid, playerGuid;
-        uint32 itemSlot, itemId, itemRandomSuffix, itemRandomPropId;
-        uint8 rollNumber, rollType;
-
-        p >> itemGuid;
-        p >> itemId;
-        p >> itemRandomSuffix;
-        p >> itemRandomPropId;
-        p >> playerGuid;
-
-        if (bot->GetGUID() != playerGuid)
-            return false;
-    }
-
     ItemIds items = SelectInventoryItemsToEquip();
     EquipItems(items);
     return true;
