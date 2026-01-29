0c8fa997630b84d47020776aa33725fce92a2cef
SmashingQuasar <nicolas.cordier@outlook.com>
2026-01-29 14:30:47 +0100

    fix: Resolved an unused parameter warning in MoveToTravelTargetAction.cpp.

--- src/Ai/Base/Actions/MoveToTravelTargetAction.cpp
+++ src/Ai/Base/Actions/MoveToTravelTargetAction.cpp
@@ -7,10 +7,9 @@
 
 #include "ChooseRpgTargetAction.h"
 #include "LootObjectStack.h"
-#include "PathGenerator.h"
 #include "Playerbots.h"
 
-bool MoveToTravelTargetAction::Execute(Event event)
+bool MoveToTravelTargetAction::Execute(Event)
 {
     TravelTarget* target = AI_VALUE(TravelTarget*, "travel target");
 
