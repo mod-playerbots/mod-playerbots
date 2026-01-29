0bfe7070ec62bb51b6173790e610e3a21f341d4d
SmashingQuasar <nicolas.cordier@outlook.com>
2026-01-29 14:25:28 +0100

    fix: Resolved all warnings in OpenItemAction.cpp and did a small refactor.

--- src/Ai/Base/Actions/OpenItemAction.cpp
+++ src/Ai/Base/Actions/OpenItemAction.cpp
@@ -7,35 +7,35 @@
 #include "LootObjectStack.h"
 #include "AiObjectContext.h"
 
-bool OpenItemAction::Execute(Event event)
+bool OpenItemAction::Execute(Event)
 {
-    bool foundOpenable = false;
+    Item* const item = botAI->FindOpenableItem();
 
-    Item* item = botAI->FindOpenableItem();
-    if (item)
+    if (item == nullptr)
     {
-        uint8 bag = item->GetBagSlot();  // Retrieves the bag slot (0 for main inventory)
-        uint8 slot = item->GetSlot();    // Retrieves the actual slot inside the bag
-
-        OpenItem(item, bag, slot);
-        foundOpenable = true;
+        return false;
     }
 
-    return foundOpenable;
+    uint8_t bag = item->GetBagSlot();  // Retrieves the bag slot (255 for main inventory)
+    uint8_t slot = item->GetSlot();    // Retrieves the actual slot inside the bag
+
+    this->OpenItem(item, bag, slot);
+
+    return true;
 }
 
-void OpenItemAction::OpenItem(Item* item, uint8 bag, uint8 slot)
+void OpenItemAction::OpenItem(Item* item, uint8_t bag, uint8_t slot)
 {
     WorldPacket packet(CMSG_OPEN_ITEM);
     packet << bag << slot;
-    bot->GetSession()->HandleOpenItemOpcode(packet);
+    this->bot->GetSession()->HandleOpenItemOpcode(packet);
 
     // Store the item GUID as the loot target
     LootObject lootObject;
     lootObject.guid = item->GetGUID();
-    botAI->GetAiObjectContext()->GetValue<LootObject>("loot target")->Set(lootObject);
+    this->botAI->GetAiObjectContext()->GetValue<LootObject>("loot target")->Set(lootObject);
 
     std::ostringstream out;
     out << "Opened item: " << item->GetTemplate()->Name1;
-    botAI->TellMaster(out.str());
+    this->botAI->TellMaster(out.str());
 }
