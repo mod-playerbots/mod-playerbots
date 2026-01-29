6c266fa181a8fa3475383080684e78d81bd6ff7a
SmashingQuasar <nicolas.cordier@outlook.com>
2026-01-29 23:07:36 +0100

    fix: Resolved all warnings in TankWarriorStrategy.cpp.

--- src/Ai/Class/Warrior/Strategy/TankWarriorStrategy.cpp
+++ src/Ai/Class/Warrior/Strategy/TankWarriorStrategy.cpp
@@ -5,8 +5,6 @@
 
 #include "TankWarriorStrategy.h"
 
-#include "Playerbots.h"
-
 class TankWarriorStrategyActionNodeFactory : public NamedObjectFactory<ActionNode>
 {
 public:
@@ -26,7 +24,7 @@ public:
     }
 
 private:
-    static ActionNode* heroic_throw_taunt(PlayerbotAI* botAI)
+    static ActionNode* heroic_throw_taunt(PlayerbotAI*)
     {
         return new ActionNode(
             "heroic throw",
@@ -36,7 +34,7 @@ private:
         );
     }
 
-    static ActionNode* heroic_throw_on_snare_target(PlayerbotAI* botAI)
+    static ActionNode* heroic_throw_on_snare_target(PlayerbotAI*)
     {
         return new ActionNode(
             "heroic throw on snare target",
@@ -46,7 +44,7 @@ private:
         );
     }
 
-    static ActionNode* last_stand(PlayerbotAI* botAI)
+    static ActionNode* last_stand(PlayerbotAI*)
     {
         return new ActionNode(
             "last stand",
@@ -56,7 +54,7 @@ private:
         );
     }
 
-    static ActionNode* devastate(PlayerbotAI* botAI)
+    static ActionNode* devastate(PlayerbotAI*)
     {
         return new ActionNode(
             "devastate",
@@ -66,7 +64,7 @@ private:
         );
     }
 
-    static ActionNode* commanding_shout(PlayerbotAI* botAI)
+    static ActionNode* commanding_shout(PlayerbotAI*)
     {
         return new ActionNode(
             "commanding shout",
@@ -76,7 +74,7 @@ private:
         );
     }
 
-    static ActionNode* sunder_armor(PlayerbotAI* botAI)
+    static ActionNode* sunder_armor(PlayerbotAI*)
     {
         return new ActionNode(
             "sunder armor",
@@ -86,7 +84,7 @@ private:
         );
     }
 
-    static ActionNode* charge(PlayerbotAI* botAI)
+    static ActionNode* charge(PlayerbotAI*)
     {
         return new ActionNode(
             "charge",
@@ -96,7 +94,7 @@ private:
         );
     }
 
-    static ActionNode* taunt(PlayerbotAI* botAI)
+    static ActionNode* taunt(PlayerbotAI*)
     {
         return new ActionNode(
             "taunt",
@@ -106,7 +104,7 @@ private:
         );
     }
 
-    static ActionNode* vigilance(PlayerbotAI* botAI)
+    static ActionNode* vigilance(PlayerbotAI*)
     {
         return new ActionNode(
             "vigilance",
@@ -116,7 +114,7 @@ private:
         );
     }
 
-    static ActionNode* enraged_regeneration(PlayerbotAI* botAI)
+    static ActionNode* enraged_regeneration(PlayerbotAI*)
     {
         return new ActionNode(
             "enraged regeneration",
