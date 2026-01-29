73304b3556f5bdae0fe8761d58059469d6dc320b
SmashingQuasar <nicolas.cordier@outlook.com>
2026-01-29 23:12:02 +0100

    fix: Resolved all warnings in DrakTharonKeepActions.cpp.

--- src/Ai/Dungeon/DraktharonKeep/Action/DrakTharonKeepActions.cpp
+++ src/Ai/Dungeon/DraktharonKeep/Action/DrakTharonKeepActions.cpp
@@ -1,8 +1,7 @@
 #include "Playerbots.h"
 #include "DrakTharonKeepActions.h"
-#include "DrakTharonKeepStrategy.h"
 
-bool CorpseExplodeSpreadAction::Execute(Event event)
+bool CorpseExplodeSpreadAction::Execute(Event)
 {
     Unit* boss = AI_VALUE2(Unit*, "find target", "trollgore");
     if (!boss) { return false; }
@@ -25,7 +24,7 @@ bool CorpseExplodeSpreadAction::Execute(Event event)
     return false;
 }
 
-bool AvoidArcaneFieldAction::Execute(Event event)
+bool AvoidArcaneFieldAction::Execute(Event)
 {
     Unit* boss = AI_VALUE2(Unit*, "find target", "novos the summoner");
     if (!boss) { return false; }
@@ -44,7 +43,7 @@ bool NovosDefaultPositionAction::isUseful()
     float threshold = 15.0f;
     return bot->GetDistance(NOVOS_PARTY_POSITION) > threshold;
 }
-bool NovosDefaultPositionAction::Execute(Event event)
+bool NovosDefaultPositionAction::Execute(Event)
 {
     float clusterDistance = 4.0f;
     // Only reposition if we're not killing anything
@@ -59,7 +58,7 @@ bool NovosDefaultPositionAction::Execute(Event event)
     return false;
 }
 
-bool NovosTargetPriorityAction::Execute(Event event)
+bool NovosTargetPriorityAction::Execute(Event)
 {
     // TODO: This can be improved, some parts are still buggy.
     // But it works for now and this fight is very easy
