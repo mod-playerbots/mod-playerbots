b0443c6922a30ba1571b1deefba06038ccb15a9f
kadeshar <kadeshar@gmail.com>
2026-02-11 20:11:31 +0100

    - Added unobtainable items to config

--- src/Mgr/Item/RandomItemMgr.cpp
+++ src/Mgr/Item/RandomItemMgr.cpp
@@ -2257,10 +2257,7 @@ void RandomItemMgr::BuildEquipCacheNew()
             continue;
         }
 
-        // Unobtainable or unusable items
-        if (itemId == 12468 || // Chilton Wand
-            itemId == 22784 || // Sunwell Orb
-            itemId == 46978) // Totem of the Earthen Ring
+        if (sPlayerbotAIConfig.unobtainableItems.find(itemId) != sPlayerbotAIConfig.unobtainableItems.end())
             continue;
 
         equipCacheNew[proto->RequiredLevel][proto->InventoryType].push_back(itemId);
