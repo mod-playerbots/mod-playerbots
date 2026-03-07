293699e7aabe16f38ab276d8282a4d84af4461fc
dillyns <49765217+dillyns@users.noreply.github.com>
2026-03-06 21:41:56 -0500

    Move dps seal actions to GenericPaladinStrategy so they can be used as fallbacks

--- src/Ai/Class/Paladin/Strategy/HealPaladinStrategy.cpp
+++ src/Ai/Class/Paladin/Strategy/HealPaladinStrategy.cpp
@@ -30,8 +30,7 @@ void HealPaladinStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
         new TriggerNode(
             "seal",
             {
-                NextAction("seal of wisdom", ACTION_HIGH + 1),
-                NextAction("seal of righteousness", ACTION_HIGH)
+                NextAction("seal of wisdom", ACTION_HIGH),
             }
         )
     );
