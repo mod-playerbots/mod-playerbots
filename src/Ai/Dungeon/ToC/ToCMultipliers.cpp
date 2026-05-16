51fb057d19d181c568570972d7d85c6cbc7a4ffd
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-05-16 00:25:27 -0700

    Proposal

---
+++ src/Ai/Dungeon/ToC/ToCMultipliers.cpp
@@ -0,0 +1,9 @@
+#include "ToCMultipliers.h"
+#include "ToCActions.h"
+#include "GenericSpellActions.h"
+#include "ChooseTargetActions.h"
+#include "MovementActions.h"
+#include "ToCTriggers.h"
+#include "Action.h"
+
+//float tocMultiplier::GetValue(Action* action) { return 1.0f; }
---
+++ src/Ai/Raid/BWL/BWLTriggerContext.h
@@ -0,0 +1,21 @@
+#ifndef _PLAYERBOT_RAIDBWLTRIGGERCONTEXT_H
+#define _PLAYERBOT_RAIDBWLTRIGGERCONTEXT_H
+
+#include "NamedObjectContext.h"
+#include "BWLTriggers.h"
+
+class RaidBwlTriggerContext : public NamedObjectContext<Trigger>
+{
+public:
+    RaidBwlTriggerContext()
+    {
+        creators["bwl suppression device"] = &RaidBwlTriggerContext::bwl_suppression_device;
+        creators["bwl affliction bronze"] = &RaidBwlTriggerContext::bwl_affliction_bronze;
+    }
+
+private:
+    static Trigger* bwl_suppression_device(PlayerbotAI* ai) { return new BwlSuppressionDeviceTrigger(ai); }
+    static Trigger* bwl_affliction_bronze(PlayerbotAI* ai) { return new BwlAfflictionBronzeTrigger(ai); }
+};
+
+#endif
