4859d25d569aa2e4005253f819e4c566ab8aceb1
avirar <avirar@users.noreply.github.com>
2026-06-15 17:16:51 +1000

    Simplify set master: always target command issuer

--- src/Ai/Base/Actions/SetMasterAction.cpp
+++ src/Ai/Base/Actions/SetMasterAction.cpp
@@ -9,56 +9,10 @@
 #include "PlayerbotAI.h"
 #include "BroadcastHelper.h"
 
-Player* SetMasterAction::GetPlayer(Event event)
-{
-    Player* player = nullptr;
-    ObjectGuid guid = event.getObject();
-
-    if (guid)
-    {
-        player = ObjectAccessor::FindPlayer(guid);
-        if (player)
-            return player;
-    }
-
-    std::string text = event.getParam();
-
-    if (!text.empty())
-    {
-        if (normalizePlayerName(text))
-        {
-            player = ObjectAccessor::FindPlayerByName(text.c_str());
-            if (player)
-                return player;
-        }
-
-        return nullptr;
-    }
-
-    Player* master = GetMaster();
-    if (!master)
-        guid = bot->GetTarget();
-    else
-        guid = master->GetTarget();
-
-    player = ObjectAccessor::FindPlayer(guid);
-    if (player)
-        return player;
-
-    player = event.getOwner();
-    if (player)
-        return player;
-
-    return nullptr;
-}
-
 bool SetMasterAction::Execute(Event event)
 {
-    Player* newMaster = GetPlayer(event);
-    if (!newMaster)
-        return false;
-
-    if (newMaster == bot)
+    Player* newMaster = event.getOwner();
+    if (!newMaster || newMaster == bot)
         return false;
 
     Group* group = bot->GetGroup();
--- src/Ai/Base/Actions/SetMasterAction.h
+++ src/Ai/Base/Actions/SetMasterAction.h
@@ -19,7 +19,7 @@ public:
     bool isUseful() override;
 
 protected:
-    Player* GetPlayer(Event event);
+ 
 };
 
 #endif
