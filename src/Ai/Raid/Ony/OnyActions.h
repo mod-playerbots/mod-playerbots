a6ab427165297c28524a5c44388e1a79ba74f899
crow <pengchengw@me.com>
2026-06-15 17:49:18 -0500

    Revert "add license"
    This reverts commit 273c4d14cb45fa559a48d4dd79fc6390946e620f.

--- src/Ai/Raid/Ony/OnyActions.h
+++ src/Ai/Raid/Ony/OnyActions.h
@@ -1,8 +1,4 @@
-/*
- * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license, you may redistribute it
- * and/or modify it under version 3 of the License, or (at your option), any later version.
- */
-
+// RaidOnyxiaActions.h
 #ifndef _PLAYERBOT_ONYACTIONS_H
 #define _PLAYERBOT_ONYACTIONS_H
 
