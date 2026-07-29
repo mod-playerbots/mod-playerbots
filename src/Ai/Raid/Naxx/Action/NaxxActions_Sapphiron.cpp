cb0fac516f707a9d29236783e24c57957afbfd91
karlverik <karlverik0912@gmail.com>
2026-07-29 16:31:46 +0800

    Imprevement Sapphiron strategy

--- src/Ai/Raid/Naxx/Action/NaxxActions_Sapphiron.cpp
+++ src/Ai/Raid/Naxx/Action/NaxxActions_Sapphiron.cpp
@@ -75,6 +75,9 @@ bool SapphironFlightPositionAction::MoveToNearestIcebolt()
     Group* group = bot->GetGroup();
     if (!group)
         return false;
+    //raid25: 3 icebolts, raid10: 2 icebolts
+    uint8 requiredIceboltCount = (bot->GetRaidDifficulty() == RAID_DIFFICULTY_25MAN_NORMAL) ? 3 : 2;
+    uint8 iceboltCount = 0; //calculate 
 
     Player* playerWithIcebolt = nullptr;
     float minDistance;
@@ -84,6 +87,7 @@ bool SapphironFlightPositionAction::MoveToNearestIcebolt()
         if (NaxxSpellIds::HasAnyAura(member, {NaxxSpellIds::Icebolt10, NaxxSpellIds::Icebolt25}) ||
             botAI->HasAura("icebolt", member, false, false, -1, true))
         {
+            ++iceboltCount;
             if (!playerWithIcebolt || minDistance > bot->GetDistance(member))
             {
                 playerWithIcebolt = member;
@@ -91,6 +95,10 @@ bool SapphironFlightPositionAction::MoveToNearestIcebolt()
             }
         }
     }
+
+    if (iceboltCount < requiredIceboltCount)
+        return false;
+
     if (playerWithIcebolt)
     {
         Unit* boss = AI_VALUE2(Unit*, "find target", "sapphiron");
@@ -99,6 +107,16 @@ bool SapphironFlightPositionAction::MoveToNearestIcebolt()
             float angle = boss->GetAngle(playerWithIcebolt);
             float posX = playerWithIcebolt->GetPositionX() + cos(angle) * 3.0f;
             float posY = playerWithIcebolt->GetPositionY() + sin(angle) * 3.0f;
+            //distance check
+            float distToIcebolt = bot->GetDistance2d(posX, posY);
+            if (distToIcebolt < 0.9f) //i think it will be a safe distance, less than half length of safe area which cause by icebolt.
+            {
+                if (botAI->IsRanged(bot))
+                {
+                    return false;//rangebot can still cast spells behind icebolt
+                }
+                return true; //prevent melee to move
+            }
             if (MoveTo(NAXX_MAP_ID, posX, posY, helper.GENERIC_HEIGHT, false, false, false, false, MovementPriority::MOVEMENT_COMBAT))
                 return true;
 
