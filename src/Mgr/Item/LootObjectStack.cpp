7593ef76d768cae8ef26031d15e7cef5309b075e
humra <matija.huremovic@gmail.com>
2026-08-07 00:35:43 +0200

    Moved the check earlier in the function

--- src/Mgr/Item/LootObjectStack.cpp
+++ src/Mgr/Item/LootObjectStack.cpp
@@ -309,6 +309,17 @@ bool LootObject::IsLootPossible(Player* bot)
     GameObject* go = botAI->GetGameObject(guid);
     if (go && (go->HasFlag(GAMEOBJECT_FLAGS, GO_FLAG_INTERACT_COND | GO_FLAG_NOT_SELECTABLE) || !go->isSpawned()))
         return false;
+	
+	//Prevents bot from getting stuck in an infinite loop of
+	//gathering herb/ore/skin -> bag too full, don't pick up -> gather again
+	bool gatheringObject = skillId == SKILL_HERBALISM || skillId == SKILL_MINING || skillId == SKILL_SKINNING || skillId == SKILL_ENGINEERING;
+	if (gatheringObject && !botAI->HasActivePlayerMaster())
+	{
+		uint8 bagUsage = botAI->GetAiObjectContext() ->GetValue<uint8>("bag space")->Get();
+
+		if (bagUsage >= 80)
+			return false;
+	}
 
     if (skillId == SKILL_NONE)
         return true;
@@ -339,17 +350,6 @@ bool LootObject::IsLootPossible(Player* bot)
     {
         return false;  // Bot is missing a skinning knife
     }
-	
-	//Prevents bot from getting stuck in an infinite loop of
-	//Gather herb/ore/skin -> bag too full, don't pick up -> gather again
-	bool gatheringObject = skillId == SKILL_HERBALISM || skillId == SKILL_MINING || skillId == SKILL_SKINNING || skillId == SKILL_ENGINEERING;
-	if (gatheringObject && !botAI->HasActivePlayerMaster())
-	{
-		uint8 bagUsage = botAI->GetAiObjectContext() ->GetValue<uint8>("bag space")->Get();
-
-		if (bagUsage >= 80)
-			return false;
-	}
 
     return true;
 }
