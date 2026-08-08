e89f843d69dcd2a25fe2ef673305cf03d635e0dd
crow <pengchengw@me.com>
2026-08-07 23:13:26 -0500

    update text ids

--- data/sql/playerbots/updates/2026_08_07_00_ai_playerbot_sunwell_texts.sql
+++ data/sql/playerbots/updates/2026_08_07_00_ai_playerbot_sunwell_texts.sql
@@ -21,37 +21,37 @@ DELETE FROM ai_playerbot_texts_chance WHERE name IN (
 INSERT INTO ai_playerbot_texts
     (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8)
 VALUES
-    (1901, 'kalecgos_tank_sent_to_spectral_realm', 'Tank %tank has been sent to the Spectral Realm. The active Kalecgos tank is %current.', 0, 0, '', '', '', '', '', '', '', '');
+    (1913, 'kalecgos_tank_sent_to_spectral_realm', 'Tank %tank has been sent to the Spectral Realm. The active Kalecgos tank is %current.', 0, 0, '', '', '', '', '', '', '', '');
 
 INSERT INTO ai_playerbot_texts
     (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8)
 VALUES
-    (1902, 'kalecgos_tank_should_enter_spectral_realm', 'Tank %tank should enter the Spectral Realm. The active Kalecgos tank is %current.', 0, 0, '', '', '', '', '', '', '', '');
+    (1914, 'kalecgos_tank_should_enter_spectral_realm', 'Tank %tank should enter the Spectral Realm. The active Kalecgos tank is %current.', 0, 0, '', '', '', '', '', '', '', '');
 
 INSERT INTO ai_playerbot_texts
     (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8)
 VALUES
-    (1903, 'kalecgos_below_twenty_percent_health', 'Kalecgos''s health is at 20%!', 0, 0, '', '', '', '', '', '', '', '');
+    (1915, 'kalecgos_below_twenty_percent_health', 'Kalecgos''s health is at 20%!', 0, 0, '', '', '', '', '', '', '', '');
 
 INSERT INTO ai_playerbot_texts
     (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8)
 VALUES
-    (1904, 'sathrovarr_health_when_kalecgos_below_twenty_percent_health', 'Sathrovarr''s health is at %sathrovarrHealth%! Don''t forget that we need to defeat them at about the same time!', 0, 0, '', '', '', '', '', '', '', '');
+    (1916, 'sathrovarr_health_when_kalecgos_below_twenty_percent_health', 'Sathrovarr''s health is at %sathrovarrHealth%! Don''t forget that we need to defeat them at about the same time!', 0, 0, '', '', '', '', '', '', '', '');
 
 INSERT INTO ai_playerbot_texts
     (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8)
 VALUES
-    (1905, 'felmyst_flight_leader', '[NAME] is now the flight phase leader. Everybody needs to stack on [NAME] during the flight phase.', 0, 0, '', '', '', '', '', '', '', '');
+    (1917, 'felmyst_flight_leader', '[NAME] is now the flight phase leader. Everybody needs to stack on [NAME] during the flight phase.', 0, 0, '', '', '', '', '', '', '', '');
 
 INSERT INTO ai_playerbot_texts
     (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8)
 VALUES
-    (1906, 'kiljaeden_designated_dragon_orb_user', '%bot is the first assistant and the designated dragon orb user!', 0, 0, '', '', '', '', '', '', '', '');
+    (1918, 'kiljaeden_designated_dragon_orb_user', '%bot is the first assistant and the designated dragon orb user!', 0, 0, '', '', '', '', '', '', '', '');
 
 INSERT INTO ai_playerbot_texts
     (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8)
 VALUES
-    (1907, 'kiljaeden_no_designated_dragon_orb_user', 'No bot has been assigned as the designated dragon orb user, and therefore a player must control the dragons. If you would like a bot to use the dragon orbs, please set the assistant flag for a bot.', 0, 0, '', '', '', '', '', '', '', '');
+    (1919, 'kiljaeden_no_designated_dragon_orb_user', 'No bot has been assigned as the designated dragon orb user, and therefore a player must control the dragons. If you would like a bot to use the dragon orbs, please set the assistant flag for a bot.', 0, 0, '', '', '', '', '', '', '', '');
 
 INSERT INTO ai_playerbot_texts_chance (name, probability) VALUES
     ('kalecgos_tank_sent_to_spectral_realm', 100),
