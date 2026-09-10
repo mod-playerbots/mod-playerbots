4b878cddcbbe6e8bc493b0d2e74df96aa686a5ed
crow <pengchengw@me.com>
2026-09-10 16:18:11 -0500

    use IN_MILLISECONDS constant for time

--- src/Ai/Base/Strategy/CastTimeStrategy.cpp
+++ src/Ai/Base/Strategy/CastTimeStrategy.cpp
@@ -45,7 +45,7 @@ float CastTimeMultiplier::GetValue(Action* action)
             return 1.0f;
         }
 
-        if (castTime > (1000 * target->GetHealth() / AI_VALUE(float, "estimated group dps")))
+        if (castTime > (IN_MILLISECONDS * target->GetHealth() / AI_VALUE(float, "estimated group dps")))
         {
             return 0.1f;
         }
