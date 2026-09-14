ef9a48a745e7b95d3d614fb62f3728c5018a7828
KrashTest <krashtest.rich@gmail.com>
2026-09-14 19:19:39 +0100

    Fix bug

--- src/Ai/Dungeon/Ramp/RampActionContext.h
+++ src/Ai/Dungeon/Ramp/RampActionContext.h
@@ -20,7 +20,8 @@ public:
             &TbcDungeonHellfireRampartsActionContext::gargolmar_mark_hellfire_watchers;
 
         // Omor the Unscarred
-        creators["omor ranged spread"] = &TbcDungeonHellfireRampartsActionContext::omor_ranged_spread;
+        creators["omor ranged spread"] =
+            &TbcDungeonHellfireRampartsActionContext::omor_ranged_spread;
 
         creators["omor treachery aura flee from players"] =
             &TbcDungeonHellfireRampartsActionContext::omor_treachery_aura_flee_from_players;
@@ -28,17 +29,24 @@ public:
         creators["omor treachery aura flee from tank"] =
             &TbcDungeonHellfireRampartsActionContext::omor_treachery_aura_flee_from_tank;
 
-        creators["omor mark fiendish hound"] = &TbcDungeonHellfireRampartsActionContext::omor_mark_fiendish_hound;
+        creators["omor mark fiendish hound"] =
+            &TbcDungeonHellfireRampartsActionContext::omor_mark_fiendish_hound;
 
         // Vazruden & Nazan
-        creators["vazruden tank position boss"] = &TbcDungeonHellfireRampartsActionContext::vazruden_tank_position_boss;
+        creators["vazruden tank position boss"] =
+            &TbcDungeonHellfireRampartsActionContext::vazruden_tank_position_boss;
 
-        creators["vazruden mark boss"] = &TbcDungeonHellfireRampartsActionContext::vazruden_mark_boss;
+        creators["vazruden mark boss"] =
+            &TbcDungeonHellfireRampartsActionContext::vazruden_mark_boss;
 
-        creators["nazan set tremor totem"] = &TbcDungeonHellfireRampartsActionContext::nazan_set_tremor_totem;
+        creators["nazan set tremor totem"] =
+            &TbcDungeonHellfireRampartsActionContext::nazan_set_tremor_totem;
 
         creators["nazan set fire resistance totem"] =
             &TbcDungeonHellfireRampartsActionContext::nazan_set_fire_resistance_totem;
+
+        creators["nazan set fire resistance aura"] =
+            &TbcDungeonHellfireRampartsActionContext::nazan_set_fire_resistance_aura;
     }
 
 private:
@@ -49,30 +57,41 @@ private:
     }
 
     // Omor the Unscarred
-    static Action* omor_ranged_spread(PlayerbotAI* botAI) { return new OmorRangedSpreadAction(botAI); }
+    static Action* omor_ranged_spread(PlayerbotAI* botAI) {
+        return new OmorRangedSpreadAction(botAI);
+    }
 
-    static Action* omor_treachery_aura_flee_from_players(PlayerbotAI* botAI)
-    {
+    static Action* omor_treachery_aura_flee_from_players(PlayerbotAI* botAI) {
         return new OmorTreacheryAuraFleeFromPlayersAction(botAI);
     }
 
-    static Action* omor_treachery_aura_flee_from_tank(PlayerbotAI* botAI)
-    {
+    static Action* omor_treachery_aura_flee_from_tank(PlayerbotAI* botAI) {
         return new OmorTreacheryAuraFleeFromTankAction(botAI);
     }
 
-    static Action* omor_mark_fiendish_hound(PlayerbotAI* botAI) { return new OmorMarkFiendishHoundAction(botAI); }
+    static Action* omor_mark_fiendish_hound(PlayerbotAI* botAI) {
+        return new OmorMarkFiendishHoundAction(botAI);
+    }
 
     // Vazruden & Nazan
-    static Action* vazruden_tank_position_boss(PlayerbotAI* botAI) { return new VazrudenTankPositionBossAction(botAI); }
+    static Action* vazruden_tank_position_boss(PlayerbotAI* botAI) {
+        return new VazrudenTankPositionBossAction(botAI);
+    }
 
-    static Action* vazruden_mark_boss(PlayerbotAI* botAI) { return new VazrudenMarkBossAction(botAI); }
+    static Action* vazruden_mark_boss(PlayerbotAI* botAI) {
+        return new VazrudenMarkBossAction(botAI);
+    }
 
-    static Action* nazan_set_tremor_totem(PlayerbotAI* botAI) { return new NazanSetTremorTotemAction(botAI); }
+    static Action* nazan_set_tremor_totem(PlayerbotAI* botAI) {
+        return new NazanSetTremorTotemAction(botAI);
+    }
 
-    static Action* nazan_set_fire_resistance_totem(PlayerbotAI* botAI)
-    {
+    static Action* nazan_set_fire_resistance_totem(PlayerbotAI* botAI) {
         return new NazanSetFireResistanceTotemAction(botAI);
+    }
+
+    static Action* nazan_set_fire_resistance_aura(PlayerbotAI* botAI) {
+        return new NazanSetFireResistanceAuraAction(botAI);
     }
 };
 
