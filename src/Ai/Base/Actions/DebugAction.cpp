8859305686183c33b441b509a5894bd3eac5263c
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-09-15 09:17:36 -0700

    Remove override

--- src/Ai/Base/Actions/DebugAction.cpp
+++ src/Ai/Base/Actions/DebugAction.cpp
@@ -468,7 +468,7 @@ bool DebugAction::Execute(Event event)
     else if (text.find("spell ") != std::string::npos)
     {
         uint32 spellEffect = stoi(text.substr(6));
-        master->SendPlaySpellVisual(bot->GetGUID(), spellEffect);
+        bot->SendPlaySpellVisual(spellEffect);
         return true;
     }
     else if (text.find("tspellmap") != std::string::npos)
