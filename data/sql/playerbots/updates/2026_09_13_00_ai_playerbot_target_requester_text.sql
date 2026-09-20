fd630fffd86e3c0b767e08fd2c28a785f10c0492
Atanas Petrov <waterdrinket@gmail.com>
2026-09-20 21:38:04 +0300

    fix(CORE/Commands): register target me as chat action and add ID to localized response

--- data/sql/playerbots/updates/2026_09_13_00_ai_playerbot_target_requester_text.sql
+++ data/sql/playerbots/updates/2026_09_13_00_ai_playerbot_target_requester_text.sql
@@ -11,10 +11,11 @@ DELETE FROM ai_playerbot_texts_chance
 WHERE name = 'target_requester_success';
 
 INSERT INTO `ai_playerbot_texts`
-(`name`, `text`, `say_type`, `reply_type`,
+(`id`, `name`, `text`, `say_type`, `reply_type`,
  `text_loc1`, `text_loc2`, `text_loc3`, `text_loc4`,
  `text_loc5`, `text_loc6`, `text_loc7`, `text_loc8`)
 VALUES (
+1913,
 'target_requester_success',
 'Targeting %target',
 0, 0,
